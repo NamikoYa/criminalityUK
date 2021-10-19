498c196bbdc7985bb0470f925526560789e9d866
himJoni <johannes.voegtlin@bbzbl-it.ch>
2021-10-19 20:56:45 +0200

    CRIM-5 | Added Category Dropdown

--- src/gui/testFile.py
+++ src/gui/testFile.py
@@ -3,25 +3,38 @@ from tkinter import *
 
 # Create object
 root = Tk()
-root.geometry("250x250")
+root.title("Crime in the UK")
+root.geometry("300x250")
 
 # Dropdown menu options
 LOCATIONOPTIONS = [
-    "Location1",
-    "Location2",
-    "Location3",
-    "Location4"
+    "London",
+    "Wilshire",
+    "Manchester",
+    "York"
+]
+
+CATEGORYOPTIONS = [
+    "Burglary",
+    "Murder",
+    "Rape",
+    "Tax evasion"
 ]
 
 # datatype of menu text
-clicked = StringVar()
+clickedLoc = StringVar()
+clickedCat = StringVar()
 
 # initial menu text
-clicked.set("Select Location")
+clickedLoc.set("Select Location")
+clickedCat.set("Select Category")
 
 # Create Dropdown menu
-drop = OptionMenu(root, clicked, *LOCATIONOPTIONS)
-drop.pack()
+drop1 = OptionMenu(root, clickedLoc, *LOCATIONOPTIONS)
+drop2 = OptionMenu(root, clickedCat, *CATEGORYOPTIONS)
+
+drop1.pack()
+drop2.pack()
 
 # Execute tkinter
 root.mainloop()
