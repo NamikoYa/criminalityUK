500a632c07c7a5be7784b46587d3c19577ea00dc
himJoni <johannes.voegtlin@bbzbl-it.ch>
2021-10-23 19:25:38 +0200

    CRIM-5 | Input/Output

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -3,13 +3,27 @@ from tkinter import *
 # Initializes Tkinter
 root = Tk()
 
-# Creating a Label Widget
+
+# Functions
+def click_category_button():
+
+    response_string = "Entered Category: " + entryCategory.get()
+    category_label = Label(root, text=response_string)
+    category_label.grid(row=2, column=0)
+
+
+# Creating Widgets
 welcomeLabel = Label(root, text="Welcome to CriminalityUK")
 instructionLabel = Label(root, text="Press the buttons")
+entryCategory = Entry(root)  # Will be dropdown later
+entryCategory.insert(0, "Enter Category:")
+categoryButton = Button(root, command=click_category_button, text="Show Category", padx=50, pady=5)
 
 # Shoving it onto the screen
 welcomeLabel.grid(row=0, column=0)
 instructionLabel.grid(row=1, column=0)
+entryCategory.grid(row=1, column=1)
+categoryButton.grid(row=2, column=1)
 
 # Keeps window running
 root.mainloop()
