c31ed957bb7fd4d49dae21f4f2521c8443be418b
NamikoYa <nati.wernli@gmail.com>
2021-10-25 01:42:02 +0200

    CRIM-7 | minor changes

--- src/app/gui/gui.py
+++ src/app/gui/gui.py
@@ -19,7 +19,7 @@ layout = [[psg.Text('Choose category of crime:', size=(40, 1), font=('Lucida', 1
           [psg.Text('')],
           [psg.Button('Generate', font=('Lucida', 12)), psg.Button('Cancel', font=('Lucida', 12))]]
 
-# create window
+# create window UI
 window = psg.Window('Graph Generator', layout)
 # event loop to process events and get values of inputs
 while True:
@@ -43,31 +43,36 @@ while True:
                 crime_rate_of_month.append(crime_rate)
             crime_rate_of_year.append(crime_rate_of_month)
 
-    data = np.array(crime_rate_of_year)
+    try:
+        crime_rate_of_year
+    except NameError:
+        print(f'crime_rate_of_year is {crime_rate}')
+    else:
+        data = np.array(crime_rate_of_year)
 
-    number_of_months = data.shape[0]
-    number_of_locations = data.shape[1]
-    for i, location_name in enumerate(list_of_locations):
-        plt.plot(np.linspace(0, 365, number_of_months), data[:, i], label=f'{location_name}')
+        number_of_months = data.shape[0]
+        number_of_locations = data.shape[1]
+        for i, location_name in enumerate(list_of_locations):
+            plt.plot(np.linspace(0, 365, number_of_months), data[:, i], label=f'{location_name}')
 
-    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    percentage = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    plt.xticks(np.linspace(0, 365, number_of_months), months[-number_of_months:])
-    plt.yticks(percentage)
-    plt.title(f"Category {values['category']}")
-    plt.xlabel(values['year'])
-    plt.ylabel('Crime rate in %')
-    plt.show()
-
-    if values['table']:
-        fig, ax = plt.subplots()
-        # hide axes
-        fig.patch.set_visible(False)
-        ax.axis('off')
-        columns = tuple(months[-number_of_months:])
-        rows = list_of_locations
-        cells = data.astype(str)
-        plt.table(cellText=cells, rowLabels=columns, colLabels=rows, loc='center')
+        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        percentage = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        plt.xticks(np.linspace(0, 365, number_of_months), months[-number_of_months:])
+        plt.yticks(percentage)
+        plt.title(f"Category {values['category']}")
+        plt.xlabel(values['year'])
+        plt.ylabel('Crime rate in %')
         plt.show()
+
+        if values['table']:
+            fig, ax = plt.subplots()
+            # hide axes
+            fig.patch.set_visible(False)
+            ax.axis('off')
+            columns = tuple(months[-number_of_months:])
+            rows = list_of_locations
+            cells = data.astype(str)
+            plt.table(cellText=cells, rowLabels=columns, colLabels=rows, loc='center')
+            plt.show()
 
 window.close()
